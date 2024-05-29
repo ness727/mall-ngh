c9a66397aa3c1a365aa68c6e0e41b7414364e9ff
ness727 <gnsdl727@naver.com>
2024-05-29 16:29:01 +0900

    first-commit

---
+++ src/test/java/idusw/springboot/egymall/EgyMallApplicationTests.java
@@ -0,0 +1,15 @@
+package idusw.springboot.egymall;
+
+import idusw.springboot.egymall.model.MemberDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class EgyMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
